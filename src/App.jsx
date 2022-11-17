5959b1f2dc685994484036b8ec3aab1e5480f882
RubenG <ramses673@hotmail.com>
2022-11-16 18:42:30 -0600

    se terminaron las funciones para comprobar las notas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [listaRemision, setListaRemision] = useState([]);
   const [remisiones, setRemisiones] = useState([]);
-  const [sumaRemsion, setSumaRemision] = useState(0);
+  const [cargando, setCargando] = useState(true);  
 
   useEffect(() => {
     
@@ -15,7 +15,9 @@ function App() {
 
     fetch('https://vercel-api-eta.vercel.app/api/remisiones')
     .then((response) => response.json())
-    .then((data) => setRemisiones(data));
+    .then((data) => setRemisiones(data))
+    .then(setCargando(false));
+
   },[])
 
   const Mostrar_listaRemision = () =>
@@ -26,8 +28,7 @@ function App() {
           <p>cliente: {item.cliente}</p> 
           <p>domicilio: {item.domicilio}</p> 
           <p>total: {item.total}</p>
-          {console.log('comparacion de sumas del folio '+ item.folio +': '+ sumaRemsion+'    '+item.total)}
-          <Mostrar_remisiones folio={item.folio}/>
+          <Mostrar_remisiones folio={item.folio} total={item.total}/>          
         </div>
       )}
     </div>
@@ -38,31 +39,56 @@ function App() {
   }
 
   const Mostrar_remisiones = (props) =>
-    <div className='Remisiones'> 
+    <div className='Remisiones'>     
       {filtrar_remisiones(props.folio).map((item_remision, index) =>
-        <div className='Remision_item' key={index}>
+        <div className='Remision_item' key={index} style= {comparar_totales(props.folio, props.total) ? {background : 'none'} : {background : 'red'}}>
           <div className='fila'>
             <p>{item_remision.cantidad}</p> 
             <p> - {item_remision.producto}</p> 
           </div>
-          <p style={{textAlign: 'end'}}>$ {item_remision.total}</p> 
-          
+          <p style={{textAlign: 'end'}}>$ {item_remision.total}</p>
         </div> 
       )}
     </div>
+  
+  const comparar_totales = (folio, total) =>{
+    let result = true;
+    let suma = 0;
+    let remisiones = filtrar_remisiones(folio);    
+    remisiones.forEach(item =>{
+      suma += parseFloat(item.total);
+    })    
+    total = parseFloat(total);    
+    total == suma ? result = true : result = false;    
+    return result;
+  }
+
+  const comprobar_estado = () =>{
+    let estado = true;
+    listaRemision.forEach(item => {
+      if (!comparar_totales(item.folio, item.total))
+        estado = false;
+    })  
+    return estado;
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React prueba</h1>
-      <Mostrar_listaRemision/>      
+    <div className="App">      
+        
+      {cargando ?           
+          <h1>Cargando...</h1>       
+      :
+        <>
+          <h1>estado de las notas: </h1>
+          {comprobar_estado() ?  
+            <h1 style={{color : 'green'}}> correcto </h1>
+          :
+            <h1 style={{color : 'red'}}> con error </h1>
+          }
+          <Mostrar_listaRemision/>
+      </>
+      }      
+      
     </div>
   )
 }
